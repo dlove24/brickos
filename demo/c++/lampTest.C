8d92f6cac976822c8024c5f9874ec3a1a8ea454c
stephmo <stephmo>
2002-09-23 03:06:04 +0000

    First mostly working version - working powerdown problem

---
+++ demo/c++/lampTest.C
@@ -0,0 +1,49 @@
+//
+// The contents of this file are subject to the Mozilla Public License
+// Version 1.0 (the "License"); you may not use this file except in
+// compliance with the License. You may obtain a copy of the License
+// at http://www.mozilla.org/MPL/
+// 
+// Software distributed under the License is distributed on an "AS IS"
+// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+// the License for the specific language governing rights and
+// limitations under the License.
+//
+// This software was developed as part of the legOS project.
+//
+// Contributor: Pat Welch (legOS@mousebrains.com)
+
+// This program beeps once a second
+
+#include <config.h>
+#if defined(CONF_DMOTOR)
+
+#include <c++/Lamp.H>
+#include <c++/Sound.H>
+
+int 
+main(int argc, 
+     char **argv) 
+{
+  Lamp myLite(Lamp::B);
+
+  unsigned char power = 0;
+  myLite.on();
+  while (power < 255) {
+	myLite.brightness(power);
+	sleep(1);
+	power += (256/16);
+  }
+  myLite.off();
+  return 0;
+}
+#else // CONF_DMOTOR
+#warning lampTest.C requires CONF_DMOTOR
+#warning lamp demo will do nothing
+int 
+main(int argc, 
+     char **argv) 
+{
+  return 0;
+}
+#endif // CONF_DMOTOR
